6d879b749a9885ff56b8ebd167c8fd1c5a07ec87
Andrey Prokopenko <9478806+terem42@users.noreply.github.com>
2021-09-10 23:43:20 +0200

    simplified

--- deploy/runme.sql
+++ deploy/runme.sql
@@ -1,17 +1,1 @@
-DROP TABLE IF EXISTS links;
-
-CREATE TABLE links (
-	id SERIAL PRIMARY KEY,
-	url VARCHAR(255) NOT NULL,
-	name VARCHAR(255) NOT NULL,
-	description VARCHAR (255),
-        last_update DATE
-);
-
-
-BEGIN
-
-INSERT INTO links (url, name)
-VALUES('https://www.postgresqltutorial.com','PostgreSQL Tutorial');
-
-COMMIT;
+select * from pg_tables;
